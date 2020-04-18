1bad73966d62f70e1b48115e7ed3726c9f3f7ea2
savior <saviorium@gmail.com>
2020-04-18 11:35:03 +0300

    Initial scaffolding

---
+++ conf.lua
@@ -0,0 +1,5 @@
+scale = 2
+function love.conf(t)
+    t.window.width = 475 * scale
+    t.window.height = 300 * scale
+end
---
+++ main.lua
@@ -0,0 +1,29 @@
+StateManager = require "lib.hump.gamestate"
+require 'conf'
+
+states = {game = require "game.states.game"}
+
+function love.load()
+    love.window.setTitle("Ludum Dare 43 Game")
+    StateManager.switch( states.game )
+end
+
+function love.draw()
+    StateManager.draw()
+end
+
+function love.update( dt )
+    StateManager.update(dt)
+end
+
+function love.mousepressed(x, y)
+    if StateManager.current().mousepressed then
+        StateManager.current():mousepressed( x, y )
+    end	
+end
+
+function love.keypressed(key)
+    if StateManager.current().keypressed then
+        StateManager.current():keypressed(key)
+    end
+end
